a5e530e7c2ed1573c97944714f973d6c0b58782c
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2025-04-18 15:57:54 +0100

    v1.8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,50 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
     title: "SEO Bunny Racing",
     description: "Race against your competitor's website!",
+    icons: {
+        icon: "/assets/favicon/apple-touch-icon.png",
+        shortcut: "/assets/favicon/apple-touch-icon.png",
+        apple: "/assets/favicon/apple-touch-icon.png",
+    },
+    openGraph: {
+        title: "SEO Bunny Racing",
+        description: "Race against your competitor's website!",
+        url: process.env.NEXT_PUBLIC_BASE_URL,
+        siteName: "SEO Bunny Racing",
+        images: [
+            {
+                url: "/assets/favicon/apple-touch-icon.png",
+                width: 512,
+                height: 512,
+                alt: "SEO Bunny Racing",
+            },
+        ],
+        locale: "en",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: false,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "SEO Bunny Racing",
+        description: "Race against your competitor's website!",
+        creator: "@niaradotai",
+        images: ["/assets/favicon/apple-touch-icon.png"],
+    },
 };
 
 export default function RootLayout({
